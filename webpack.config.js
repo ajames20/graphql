7c22217a345e68b907ad0a0d77968a601ac435c3
Andrew James <ajames20@gmail.com>
2017-12-22 13:13:27 +0200

    add webpack config for images

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: ['url-loader?limit=10000', 'img-loader'],
+      },
     ],
   },
   plugins: [
