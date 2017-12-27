d6c511ec1eb39e12c499bcc2a91d73a94d0b99d4
Andrew James <ajames20@gmail.com>
2017-12-27 09:23:14 +0200

    add daily forecast and updat styles for dashboard

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -14,6 +14,8 @@ class Dashboard extends Component {
       country: '',
       weatherIcon: undefined,
       summary: '',
+      temperature: '',
+      daily: [],
     };
   }
 
@@ -36,7 +38,7 @@ class Dashboard extends Component {
   }
 
   getWeather(lat, lng) {
-    const uri = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/e3dc1508c16f644095ba9353d7b468a8/${lat},${lng}/`;
+    const uri = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/e3dc1508c16f644095ba9353d7b468a8/${lng},${lat}/`;
     axios
       .get(uri)
       .then(weatherData => {
@@ -49,31 +51,93 @@ class Dashboard extends Component {
 
   displayWeather(weatherData) {
     const { summary } = weatherData.data.currently;
-    let { icon } = weatherData.data.currently;
+    let { icon, temperature } = weatherData.data.currently;
+    let daily = weatherData.data.daily.data;
+
+    temperature = `${Math.round(temperature)}°`;
     icon = icon.replace(/\-/g, '_').toUpperCase();
 
-    this.setState({ summary, weatherIcon: icon });
+    this.setState({ summary, weatherIcon: icon, temperature, daily });
   }
 
   componentDidMount() {
     this.getLocation();
   }
 
+  renderDay(arr) {
+    if (!arr) {
+      return (
+        <Loader
+          className="dashboard__current-location-panel-details--loader"
+          color="#bbd2c5"
+          size="56px"
+          margin="0 auto"
+        />
+      );
+    }
+    return arr.map((day, i) => {
+      const options = {
+        weekday: 'long',
+      };
+      const dayDefaults = {
+        icon: '',
+        color: '#292e49',
+        size: 50,
+        animate: true,
+      };
+      const weekday = new Date(day.time * 1000)
+        .toLocaleTimeString('en-us', options)
+        .split(' ')[0];
+
+      const min = `${Math.round(day.temperatureMin)}°`;
+      const max = `${Math.round(day.temperatureMax)}°`;
+      const dailyWeatherIcon = day.icon.replace(/\-/g, '_').toUpperCase();
+
+      return (
+        <div className="dashboard__location-panel--list">
+          <li className="dashboard__location-panel--list-item" key={i}>
+            {weekday}
+          </li>
+          <div className="dashboard__location-panel--list-temp">
+            <span className="low">{min}</span>
+            <span className="high"> {max}</span>
+          </div>
+          <div className="dashboard__location-panel--list-icon">
+            <ReactAnimatedWeather
+              icon={dailyWeatherIcon}
+              size={dayDefaults.size}
+              color={dayDefaults.color}
+            />
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
     const defaults = {
       icon: '',
       color: '#292e49',
-      size: 246,
+      size: 230,
       animate: true,
     };
 
-    const { city, country, summary, weatherIcon } = this.state;
+    const {
+      city,
+      country,
+      summary,
+      weatherIcon,
+      temperature,
+      daily,
+    } = this.state;
     const { icon, color, size, animate } = defaults;
 
     return (
       <div className="dashboard">
         <div className="dashboard__location-panel">
-          <h2>Locations</h2>
+          <h2 className="dashboard__location-panel-header">Daily</h2>
+
+          <ul>{this.renderDay(daily)}</ul>
         </div>
         <div className="dashboard__current-location-panel">
           <div className="dashboard__current-location-panel--header">
@@ -100,8 +164,13 @@ class Dashboard extends Component {
               animate={animate}
             />
           )}
-          <p className="dashboard__current-location-panel-details--summary">
-            {summary}
+
+          <p className="dashboard__current-location-panel-details--temperature">
+            Currently: {temperature}
+          </p>
+
+          <p className="dashboard__current-location-panel-details--condition">
+            Condition: {summary}
           </p>
         </div>
       </div>
