377185842474fefe72c750a6a874f43c67ab97e6
Andrew James <ajames20@gmail.com>
2017-12-28 09:25:02 +0200

    update server

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const app = require('./server/server');
+const path = require('path');
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
 
 app.listen(process.env.PORT || 4000, () => {
   console.log('Listening');
