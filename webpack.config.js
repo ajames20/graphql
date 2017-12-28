e8c3c8159d45a9f82a4ec01c02cb247803e6c8d0
Andrew James <ajames20@gmail.com>
2017-12-28 07:17:28 +0200

    udpate webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './client/index.js',
   output: {
-    filename: './dist/bundle.min.js',
+    filename: './client/bundle.min.js',
   },
   module: {
     rules: [
