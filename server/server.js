5bd0e9c73187b25e10e23dabe78925c9f6117f89
Andrew James <ajames20@gmail.com>
2017-12-28 06:10:25 +0200

    remove plugins

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
-// const path = require('path');
+
 if (process.env.NODE_ENV !== 'production') require('dotenv').config('.env');
 
 // Create a new Express application
@@ -60,8 +60,6 @@ app.use(
     graphiql: true,
   }),
 );
-
-// app.use(express.static(path.join(__dirname, '/')));
 
 // Webpack runs as a middleware.  If any request comes in for the root route ('/')
 // Webpack will respond with the output of the webpack process: an HTML file and
